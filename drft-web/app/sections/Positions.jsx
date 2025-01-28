e50216ccb6bd15a3dbd988b41c68adaf32f8d30a
Sandinu Pinnawala <74131240+Sandinu@users.noreply.github.com>
2025-01-28 18:38:58 +0530

    Career mobile done

--- drft-web/app/sections/Positions.jsx
+++ drft-web/app/sections/Positions.jsx
@@ -6,9 +6,9 @@ const Positions = () => {
     <div className='relative w-full px-6 lg:px-40 lg:mt-28 mt-28 lg:py-10 mb-32 z-20 items-center justify-center text-center'>
       <div>
         <h1 className='text-white text-5xl font-semibold'>What We Excel At</h1>
-        <h4 className=' text-gray-300 text-lg mt-3'>At DRFT Global, we specialize in connecting clients with <br /> top-tier talent in diverse fields:</h4>
+        <h4 className=' text-gray-300 lg:text-lg lg:px-72 px-8  mt-3'>At DRFT Global, we specialize in connecting clients with top-tier talent in diverse fields:</h4>
       </div>
-      <div className='mt-8 px-40'>
+      <div className='mt-8 lg:px-40'>
         <CareerBlock title={"IT"}/>
         <CareerBlock title={"Finance"}/>
         <CareerBlock title={"Operations"}/>
--- drft-web/components/CareerBlock.jsx
+++ drft-web/components/CareerBlock.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CareerBlock = ({title}) => {
 return (
-    <div className='bg-brandBlack border border-orangeLight px-5 py-2 text-white inline-block rounded-xl mx-3 mb-5 hover:scale-105 cursor-default hover:shadow-md hover:shadow-orangeLight/30'>
+    <div className='bg-brandBlack border border-orangeLight px-5 py-2 text-white inline-block rounded-xl lg:mx-3 mx-2 mb-5 hover:scale-105 cursor-default hover:shadow-md hover:shadow-orangeLight/30'>
         <div className='flex items-center justify-center'>
             <div className='bg-orangeLight w-2 h-2 mr-2'></div>
             <h1 className='text-2xl'>{title}</h1>
