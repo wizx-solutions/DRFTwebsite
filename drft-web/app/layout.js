61e595c0dafcfc4e3f0ffc100c309b63ace769af
Sandinu Pinnawala <74131240+Sandinu@users.noreply.github.com>
2025-02-04 14:38:32 +0530

    Meta tags updated

--- drft-web/app/layout.js
+++ drft-web/app/layout.js
@@ -11,6 +11,11 @@ export default function RootLayout({children}) {
         <html lang="en">
         <head>
             <link rel="icon" href="/drft-web/app/favicon.ico"/>
+            <meta property="og:title" content="DRFT Global"/>
+            <meta property="og:description" content="Fueling Talent with limitless reach"/>
+            <meta property="og:image" content="/Meta.png" />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://drftglobal.com" />
         </head>
         <body
             className={`Montserrat antialiased`}
